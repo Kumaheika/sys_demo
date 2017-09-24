500337e0c883535a52b467ba3197cf6c03d98f27
DESKTOP-NTFSA9L\kumaheika <w87502047@gmail.com>
2017-09-24 19:08:58 +0800

    v1.3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(function () {
-    $('.dropdown-toggle').dropdown();
+    $menu = $('.menu'),
+    $menu_btn = $menu.children('.menu_btn'),
+    $content = $('.content'),
+    $content_con = $content.find('.container-fluid');
+
+    $menu_btn.each(function (index) {
+        $(this).on('click',{id:index}, toggle_con);
+    });
+    function toggle_con (e) {
+        // alert(e.data.id);
+        $num = e.data.id;
+        $menu_btn.removeClass('active');
+        $(this).addClass('active');
+        $content_con.find('.row').removeClass('active');
+        $content_con.find('.row').eq($num).addClass('active');
+    }
 })
