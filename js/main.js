11660385196d9efa2bc93744ae863d70fd37b195
kumaheika <w87502047@gmail.com>
2017-10-18 20:14:16 +0800

    v3 1024 match

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    // menu 邏輯操作
     $menu = $('.menu'),
         $menu_btn = $menu.children('.menu_btn'),
         $content = $('.content'),
@@ -16,7 +18,7 @@ $(function () {
         $content_con.find('.row').eq($num).addClass('active');
     }
 
-    //物件操作panel control
+    // panel control 物件操作
     var $panel_compo = $('.panel_compo'),
         $panel_btn = $panel_compo.find('.panel_item');
     $panel_btn.each(function (index) {
